9b31a3c7d8ec2b3c9fe03b9baea8485b3aa3e242
Tristan Lievens <tristan.lievens@gmail.com>
2016-09-09 18:22:00 +0200

    Finish team update reducer

--- src/client/reducers/index.ts
+++ src/client/reducers/index.ts
@@ -2,18 +2,21 @@ import { Action, Reducer, combineReducers } from 'redux'
 import { LoginState, loginReducer as login } from './login'
 import { LocationState, locationReducer as location } from './location'
 import { lastScriptReducer as lastScript } from './lastScript'
+import { TeamState, teamReducer as team } from './team'
 
 export interface State {
   login: LoginState
   location: LocationState
   lastAction: Action
-  lastScript: number
+  lastScript: number,
+  team: TeamState
 }
 
 export const rootReducer = combineReducers<State>({
   login,
   location,
   lastScript,
-  lastAction: (state: Action = { type: 'INIT' }, action: Action) => action
+  lastAction: (state: Action = { type: 'INIT' }, action: Action) => action,
+  team
 })
 
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import { startBot, onlyLogin } from './bot'
 const theArguments = process.argv.slice(2)
 
 start()
-.then(startBot)
-// .then(onlyLogin)
+// .then(startBot)
+.then(onlyLogin)
 .then(() => console.log('done!'))
 .catch(err => console.log("something bad errored", err))
 
